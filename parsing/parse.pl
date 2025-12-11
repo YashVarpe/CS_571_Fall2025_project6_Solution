9d611fd4b2797f51eb5a9ad3bc30dd55a6296e92
YashVarpe <yashvarpe7@gmail.com>
2025-12-10 23:12:59 -0500

    Final code

--- parsing/parse.pl
+++ parsing/parse.pl
@@ -3,7 +3,36 @@
 % Your code here:
 %%%%%%%%%%%%%%%%%
 
-parse(X) :- ????
+parse(X) :- 
+
+% Grammar: Lines -> Line ; Lines | Line
+lines(In, Out) :-
+    line(In, Rem),
+    (   match(';', Rem, Rem2)
+    ->  lines(Rem2, Out)  % Recursive: found ';', parse more
+    ;   Out = Rem         % Base: finished
+    ).
+
+% Grammar: Line -> Num , Line | Num
+line(In, Out) :-
+    num(In, Rem),
+    (   match(',', Rem, Rem2)
+    ->  line(Rem2, Out)   % Recursive: found ',', parse more
+    ;   Out = Rem         % Base: finished
+    ).
+
+% Grammar: Num -> Digit | Digit Num
+num(In, Out) :-
+    digit(In, Out).
+num(In, Out) :-
+    digit(In, Rem),
+    num(Rem, Out).
+
+% Grammar: Digit -> 0 | 1 ... | 9
+digit([H|T], T) :-
+    member(H, ['0','1','2','3','4','5','6','7','8','9']).
+
+match(H, [H|T], T).
 
 % Example execution:
 % ?- parse(['3', '2', ',', '0', ';', '1', ',', '5', '6', '7', ';', '2']).
--- search/search.pl
+++ search/search.pl
@@ -2,4 +2,42 @@
 % Your code goes here:
 %%%%%%%%%%%%%%%%%%%%%%
 
-search(Actions) :- ???
+search(Actions) :-
+    initial(StartRoom),
+    bfs([[state(StartRoom, []), []]], [state(StartRoom, [])], Actions).
+
+bfs([[state(Room, _), Path]|_], _, Path) :-
+    treasure(Room).
+
+bfs([ [state(Room, Keys), Path] | RestQueue], Visited, Actions) :-
+    findall(
+        [state(NextRoom, NewKeys), NewPath],
+        (
+            (door(Room, NextRoom); door(NextRoom, Room)),
+            
+            ( (locked_door(Room, NextRoom, Color); locked_door(NextRoom, Room, Color)) ->
+                member(Color, Keys) % Must have key if locked
+            ;
+                true % Door is not locked
+            ),
+            
+            findall(K, key(NextRoom, K), FoundKeys),
+            append(FoundKeys, Keys, AllKeys),
+            sort(AllKeys, NewKeys), % Sort to keep key list canonical
+            
+            \+ member(state(NextRoom, NewKeys), Visited),
+            
+            append(Path, [move(Room, NextRoom)], NewPath)
+        ),
+        Children
+    ),
+
+    extract_states(Children, ChildStates),
+    append(Visited, ChildStates, NewVisited),
+    append(RestQueue, Children, NewQueue),
+    
+    bfs(NewQueue, NewVisited, Actions).
+
+extract_states([], []).
+extract_states([[State, _]|T], [State|Rest]) :-
+    extract_states(T, Rest).
