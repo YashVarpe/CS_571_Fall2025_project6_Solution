ccaed771b945c806b4f9e41327e19ea3cb040fd0
YashVarpe <yashvarpe7@gmail.com>
2025-12-10 23:55:55 -0500

    .

--- parsing/parse.pl
+++ parsing/parse.pl
@@ -2,7 +2,6 @@
 % Your code here:
 %%%%%%%%%%%%%%%%%
 
-% Main entry point
 parse(X) :- lines(X, []).
 
 % Grammar: Lines -> Line ; Lines | Line
@@ -22,7 +21,6 @@ line(In, Out) :-
     ).
 
 % Grammar: Num -> Digit Num | Digit
-% We prioritize the recursive case (Digit Num) to greedily match multiple digits.
 num(In, Out) :-
     digit(In, Rem),
     num(Rem, Out).
@@ -31,15 +29,9 @@ num(In, Out) :-
 
 % Grammar: Digit -> 0 | 1 ... | 9
 digit([H|T], T) :-
-    member(H, ['0','1','2','3','4','5','6','7','8','9']).
+    my_member(H, ['0','1','2','3','4','5','6','7','8','9']).
 
-% Helper to match a specific token
 match(H, [H|T], T).
 
-% Example execution provided in comments for verification:
-% ?- parse(['3', '2', ',', '0', ';', '1', ',', '5', '6', '7', ';', '2']).
-% true.
-% ?- parse(['3', '2', ',', '0', ';', '1', ',', '5', '6', '7', ';', '2', ',']).
-% false.
-% ?- parse(['3', '2', ',', ';', '0']).
-% false.
+my_member(X, [X|_]).
+my_member(X, [_|T]) :- my_member(X, T).
